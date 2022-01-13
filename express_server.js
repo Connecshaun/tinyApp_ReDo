c9b8986e5d85fc04c5f2761c01d7531ffeed26df
Shaun Robinson <connecshaun@gmail.com>
2022-01-13 06:13:17 +0000

    Add /urls route to render a template and added more comments

--- express_server.js
+++ express_server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; 
-
 app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//templateVars must always be an object so that the data can be properly accessed by our ejs templates... which can access the value of keys
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
+
+//renders all urls along with their shortened form
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase }
+  res.render('urls_index', templateVars)
+})
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
